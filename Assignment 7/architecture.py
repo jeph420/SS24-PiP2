6ee46866fe883fcabf5c85692fd262331feff84d
OlegBushtyrkov <bushtyrkov.ol@gmail.com>
2024-07-13 20:13:19 +0200

    First few tries at submitting the Project, as well as (what I believe is) the final version of the project.

--- Assignment 7/architecture.py
+++ Assignment 7/architecture.py
@@ -44,7 +44,8 @@ class MyCNN(nn.Module):
         x = self.linear(x)
         return x
 
-model = MyCNN(1, [32, 64, 128], 3)
+target_device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = MyCNN(1, [32, 64, 128], 3).to(target_device)
 
 ############ HELPER FUNCTIONS ############
 def set_seed(seed: int = 42) -> None:
@@ -75,6 +76,7 @@ class TransformedData(Dataset):
             length = len(given_transforms)
             chosen_transforms = []
             for _ in range(2):
+                set_seed(42)
                 i = random.randint(0,length-1)
                 chosen_transforms.append(self.transform[i])
                 given_transforms.pop(i)
@@ -99,8 +101,9 @@ train_transformations = [
 ## Load the datasets
 original_data = ds.ImagesDataset(image_dir="training_data", dtype=int)
 
+set_seed(42)
 transformed_images = TransformedData(original_data.image_filepaths, train_transformations)
-transformed_gray_images = [ds.to_grayscale(np.array(image)) for image in transformed_images]
+transformed_gray_images = [ds.to_grayscale(np.array(image, dtype=np.float32)) for image in transformed_images]
 prepared_images = [ds.prepare_image(image, 100, 100, 0, 0, 32)[0] for image in transformed_gray_images]
 normalized_images = [torch.tensor(i, dtype=torch.float32)/225.0 for i in prepared_images]
 transformed_data = [(normalized_images[i], original_data[i][1], original_data[i][2], original_data[i][3]) for i in range(len(normalized_images))]
@@ -113,14 +116,14 @@ trainable_params = [i for i in model.parameters()]
 ## Create dataset splits for Training, Validation, and Testing
 batch_size = 64
 split_size = len(data)//100
-train_split, test_split, val_split = 10*split_size, 5*split_size, 5*split_size
+train_split, test_split, val_split = 85*split_size, 15*split_size, 0*split_size
 
 set_seed(42)
-data_train, data_test, data_val, _ = torch.utils.data.random_split(data, [0.1, 0.05, 0.05, 0.8])
+data_train, data_val, data_test = torch.utils.data.random_split(data, [0.80, 0.10, 0.10])
 
 ## Choose suitable Optimizer and No. Epochs
 optimizer = torch.optim.Adam(trainable_params, lr=0.001, weight_decay=0.0001)
-num_epochs = 10
+num_epochs = 20
 
 ## Set Seed for reproduceability and split the Dataset 'Randomly'
 set_seed(42)
@@ -139,10 +142,16 @@ test_loader = DataLoader(data_test, batch_size, test_sample)
 
 # Train and Validate the CNN model 
 
-i = 0
+train_loss = []
+val_loss = []
+
 for epoch in range(num_epochs):
         
-        print(f"Epoch {i} ...")
+        if len(val_loss) > 0:
+            min_loss = min(val_loss)
+        if epoch > 5 and min_loss not in val_loss[epoch-5:epoch]:
+            break
+    
         model.train()
         batch_loss = 0
         ## Loop over the DataLoader
@@ -150,15 +159,18 @@ for epoch in range(num_epochs):
         for data in train_loader:
         ## Split the train_loader into inputs and targets (the inputs will be fed to the network)
             inputs, targets, _, _= data
+            inputs, targets = inputs.to(target_device), targets.to(target_device)
            
         ## Feed the inputs to the network, and retrieve outputs (don't forget to reset the gradients)
             optimizer.zero_grad()
             outputs = model(inputs)
         ## Compute Cross-Entropy Loss and Gradients of each batch.
             loss = torch.nn.functional.cross_entropy(outputs, targets)
+            batch_loss += loss
             loss.backward()
         ## Update network's weights according to the loss (above step).
             optimizer.step()
+        train_loss.append(batch_loss/len(train_loader))
         
         ### Iterate over the entire eval_data - compute and store the loss of the data.
         model.eval()
@@ -167,20 +179,36 @@ for epoch in range(num_epochs):
             for data in val_loader:
                 ## Split the eval_loader into inputs and targets.
                 inputs, targets, _, _= data
+                inputs, targets = inputs.to(target_device), targets.to(target_device)
         
                 ## Feed the inputs to the network, and retrieve outputs.
                 outputs = model(inputs)
                 ## Compute and accumilate the loss of the batch.
                 loss = torch.nn.functional.cross_entropy(outputs, targets)
-                
-            i+=1
+                batch_loss += loss
+                new_loss = batch_loss/len(val_loader)   
+            val_loss.append(new_loss)
+            ### Save the model with the lowest loss so far
+            if len(val_loss) == 1:
+                torch.save(model.state_dict(), "model.pth")
+            elif min(val_loss) == new_loss:
+                torch.save(model.state_dict(), "model.pth")
 
-print("Testing...")
+#data_test = torch.utils.data.random_split(data, [1.00])
+#test_loader = DataLoader(data_test, batch_size)
+model = MyCNN(1, [32, 64, 128], 3).to(target_device)  
+model.load_state_dict(torch.load('model.pth'))
+model.eval()
+#print("Testing...")
+#for i in test_loader:
+#    print(i)
+
 with torch.no_grad():
     TruePositives = 0
     Total = 0
     for data in test_loader:
         inputs, targets, _, _ = data
+        inputs, targets = inputs.to(target_device), targets.to(target_device)
         outputs = model(inputs)
         prediction_batch = torch.argmax(nn.functional.softmax(outputs, dim=1), dim=1)
         for i in range(len(prediction_batch)):
@@ -188,3 +216,5 @@ with torch.no_grad():
             if prediction_batch[i] == targets[i]:
                 TruePositives += 1
     print(f"Accuracy: {100*(TruePositives/Total):.3f}%")
+
+#torch.save(model.state_dict(), "model.pth")
