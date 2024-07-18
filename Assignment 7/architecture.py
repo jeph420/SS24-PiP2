43b802ae44e6fa4e0cf90bc6a117be13fcdc6ba3
OlegBushtyrkov <bushtyrkov.ol@gmail.com>
2024-07-18 17:51:36 +0200

    Final version of the model, along with all previous versions

--- Assignment 7/architecture.py
+++ Assignment 7/architecture.py
@@ -57,10 +57,8 @@ def set_seed(seed: int = 42) -> None:
     random.seed(seed)
     np.random.seed(seed)
     torch.random.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
 
-class TransformedData(Dataset):
+class TransformedImages(Dataset):
     def __init__(self, image_paths, transforms):
         self.image_paths = image_paths
         self.transform = transforms
@@ -90,25 +88,25 @@ train_transformations = [
     transforms.GaussianBlur(3),
     transforms.RandomRotation(22.5),
     transforms.RandomHorizontalFlip(0.5),
-
 ]
 
-#test_transformations = transforms.Compose([
-#    transforms.PILToTensor(),
-#    transforms.Normalize()
-#])
 
-## Load the datasets
+## Load the original dataset
 original_data = ds.ImagesDataset(image_dir="training_data", dtype=int)
 
-set_seed(42)
-transformed_images = TransformedData(original_data.image_filepaths, train_transformations)
-transformed_gray_images = [ds.to_grayscale(np.array(image, dtype=np.float32)) for image in transformed_images]
-prepared_images = [ds.prepare_image(image, 100, 100, 0, 0, 32)[0] for image in transformed_gray_images]
-normalized_images = [torch.tensor(i, dtype=torch.float32)/225.0 for i in prepared_images]
-transformed_data = [(normalized_images[i], original_data[i][1], original_data[i][2], original_data[i][3]) for i in range(len(normalized_images))]
+## Create duplicates of the original dataset and apply aforementioned transformations
+datasets = [original_data]
+for _ in range(5):
 
-data = ConcatDataset([original_data, transformed_data])
+    set_seed(42)
+    transformed_images = TransformedImages(original_data.image_filepaths, train_transformations)
+    transformed_gray_images = [ds.to_grayscale(np.array(image, dtype=np.float32)) for image in transformed_images]
+    prepared_images = [ds.prepare_image(image, 100, 100, 0, 0, 32)[0] for image in transformed_gray_images]
+    normalized_images = [torch.tensor(i, dtype=torch.float32)/225.0 for i in prepared_images]
+    transformed_data = [(normalized_images[i], original_data[i][1], original_data[i][2], original_data[i][3]) for i in range(len(normalized_images))]
+    datasets.append(transformed_data)
+
+data = ConcatDataset(datasets)
 
 ## Create loss collectors
 trainable_params = [i for i in model.parameters()]
@@ -117,7 +115,6 @@ trainable_params = [i for i in model.parameters()]
 batch_size = 64
 split_size = len(data)//100
 train_split, test_split, val_split = 85*split_size, 15*split_size, 0*split_size
-
 set_seed(42)
 data_train, data_val, data_test = torch.utils.data.random_split(data, [0.80, 0.10, 0.10])
 
@@ -131,17 +128,12 @@ train_sample = torch.utils.data.SubsetRandomSampler(range(train_split))
 val_sample = torch.utils.data.SubsetRandomSampler(range(train_split, train_split+val_split))
 test_sample = torch.utils.data.SubsetRandomSampler(range(len(data)-test_split, len(data)))
 
+## Create DataLoaders for each data split
 train_loader = DataLoader(data_train, batch_size, train_sample)
 val_loader = DataLoader(data_val, batch_size, val_sample)
 test_loader = DataLoader(data_test, batch_size, test_sample)
 
-#print(train_split, test_split, val_split)
-#print(len(data_train), len(data_test), len(data_val))
-#print(len(train_sample), len(test_sample), len(val_sample))
-#print(len(train_loader), len(test_loader), len(val_loader))
-
 # Train and Validate the CNN model 
-
 train_loss = []
 val_loss = []
 
@@ -154,67 +146,75 @@ for epoch in range(num_epochs):
     
         model.train()
         batch_loss = 0
-        ## Loop over the DataLoader
-    
+
+        ## Loop over train_loader
         for data in train_loader:
-        ## Split the train_loader into inputs and targets (the inputs will be fed to the network)
+
+            ## Split the train_loader into inputs and targets (the inputs will be fed to the network)
             inputs, targets, _, _= data
             inputs, targets = inputs.to(target_device), targets.to(target_device)
            
-        ## Feed the inputs to the network, and retrieve outputs (don't forget to reset the gradients)
+            ## Feed the inputs to the network, and retrieve outputs (don't forget to reset the gradients)
             optimizer.zero_grad()
             outputs = model(inputs)
-        ## Compute Cross-Entropy Loss and Gradients of each batch.
+
+            ## Compute Cross-Entropy Loss and Gradients of each batch.
             loss = torch.nn.functional.cross_entropy(outputs, targets)
             batch_loss += loss
             loss.backward()
-        ## Update network's weights according to the loss (above step).
+
+            ## Update network's weights according to the loss (above step).
             optimizer.step()
+
         train_loss.append(batch_loss/len(train_loader))
         
-        ### Iterate over the entire eval_data - compute and store the loss of the data.
+        ## Iterate over val_loader - compute and store the loss of the data.
         model.eval()
         batch_loss = 0
+
         with torch.no_grad():
             for data in val_loader:
+
                 ## Split the eval_loader into inputs and targets.
                 inputs, targets, _, _= data
                 inputs, targets = inputs.to(target_device), targets.to(target_device)
         
                 ## Feed the inputs to the network, and retrieve outputs.
                 outputs = model(inputs)
+
                 ## Compute and accumilate the loss of the batch.
                 loss = torch.nn.functional.cross_entropy(outputs, targets)
                 batch_loss += loss
-                new_loss = batch_loss/len(val_loader)   
+                new_loss = batch_loss/len(val_loader)
+
             val_loss.append(new_loss)
+
             ### Save the model with the lowest loss so far
             if len(val_loss) == 1:
                 torch.save(model.state_dict(), "model.pth")
             elif min(val_loss) == new_loss:
-                torch.save(model.state_dict(), "model.pth")
+                torch.save(model.state_dict(), "model.pth") 
 
-#data_test = torch.utils.data.random_split(data, [1.00])
-#test_loader = DataLoader(data_test, batch_size)
-model = MyCNN(1, [32, 64, 128], 3).to(target_device)  
-model.load_state_dict(torch.load('model.pth'))
-model.eval()
-#print("Testing...")
-#for i in test_loader:
-#    print(i)
+########### CODE USED TO TEST MODEL ACCURACY ###########
 
-with torch.no_grad():
-    TruePositives = 0
-    Total = 0
-    for data in test_loader:
-        inputs, targets, _, _ = data
-        inputs, targets = inputs.to(target_device), targets.to(target_device)
-        outputs = model(inputs)
-        prediction_batch = torch.argmax(nn.functional.softmax(outputs, dim=1), dim=1)
-        for i in range(len(prediction_batch)):
-            Total += 1
-            if prediction_batch[i] == targets[i]:
-                TruePositives += 1
-    print(f"Accuracy: {100*(TruePositives/Total):.3f}%")
+#model = MyCNN(1, [32, 64, 128], 3).to(target_device)  
+#model.load_state_dict(torch.load('model.pth'))
+#model.eval()
 
-#torch.save(model.state_dict(), "model.pth")
+#with torch.no_grad():
+#    TruePositives = 0
+#    Total = 0
+#    for data in test_loader:
+#        inputs, targets, _, _ = data
+#        inputs, targets = inputs.to(target_device), targets.to(target_device)
+#        outputs = model(inputs)
+#        prediction_batch = torch.argmax(nn.functional.softmax(outputs, dim=1), dim=1)
+#        for i in range(len(prediction_batch)):
+#            Total += 1
+#            if prediction_batch[i] == targets[i]:
+#                TruePositives += 1
+#    print(f"Accuracy: {100*(TruePositives/Total):.3f}%")
+
+## BEST SO FAR: Accuracy: 88.742% ##
+### NEW BEST!: Accuracy: 97.463% ###
+## FINAL BEST!: Accuracy: 99.987% ##
